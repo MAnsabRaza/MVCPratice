ff22edd080c6531b41db092d2dd7d83781e4516b
MAnsabRaza <ansabnasir153@gmail.com>
2025-08-09 15:25:03 +0500

    1

--- MVC Pratice/Controllers/BatchController.cs
+++ MVC Pratice/Controllers/BatchController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVC_Pratice.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -13,5 +14,6 @@ namespace MVC_Pratice.Controllers
         {
             return View();
         }
+    
     }
 }
--- MVC Pratice/Controllers/CourseController.cs
+++ MVC Pratice/Controllers/CourseController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using MVC_Pratice.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,10 +10,71 @@ namespace MVC_Pratice.Controllers
 {
     public class CourseController : Controller
     {
-        // GET: Course
+        private AppDbContext db = new AppDbContext();
         public ActionResult Course()
         {
-            return View();
+            ViewBag.Course = db.Course.ToList();
+            return View(new Course());
         }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Course course)
+        {
+            if (Session["userId"] == null)
+            {
+                return RedirectToAction("Login", "Auth");
+            }
+            if (ModelState.IsValid)
+            {
+                if (course.Id > 0)
+                {
+                    var existingCourse = db.Course.Find(course.Id);
+                    if (existingCourse != null)
+                    {
+                        existingCourse.current_date = course.current_date;
+                        existingCourse.course_name = course.course_name;
+                        existingCourse.userId = Convert.ToInt32(Session["userId"]);
+                        db.Entry(existingCourse).State = EntityState.Modified;
+                        db.SaveChanges();
+                    }
+                }
+                else
+                {
+                    course.userId = Convert.ToInt32(Session["userId"]);
+                    course.current_date = DateTime.Now;
+                    db.Course.Add(course);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Course");
+            }
+            ViewBag.Course=db.Course.ToList();
+            return View("Course",course);
+
+        }
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var course=db.Course.Find(id);
+            if (course == null)
+            {
+                return HttpNotFound();
+            }
+            db.Course.Remove(course);
+            db.SaveChanges();
+            return RedirectToAction("Course");
+
+        }
+        [HttpGet]
+        public ActionResult Edit(int id)
+        {
+            var course=db.Course.Find(id);
+            if(course == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Course = db.Course.ToList();
+            return View("Course", course);
+        }
+
     }
 }
